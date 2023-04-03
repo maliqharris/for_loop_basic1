271df907a58df71ab5c62ccadfe5e0b723d95e94
maliqharris <127056176+maliqharris@users.noreply.github.com>
2023-04-03 14:34:30 -0400

    Add files via upload
    For Loops in Python!

---
+++ for_loop_basic1.py
@@ -0,0 +1,30 @@
+# for i in range(151):
+#     print(i)
+    
+# for i in range(5,1001, 5):
+#     print(i)
+
+# for i in range(1, 101):
+#     if i % 5 == 0:
+#         print ("Coding")
+#     else:
+#         print(i)
+
+# temp = 0
+# for i in range(500000):
+#     if i % 2 != 0:
+#         temp = i + temp
+# print(temp)
+        
+# for i in range (2018,0,-4):
+#     print(i)
+
+
+
+
+lowNume = 0
+highNum = 9
+mult = 3
+for lowNum in range(highNum):
+    if(lowNum % mult == 0):
+        print(lowNum)
